86edc0e33e558350685fff0d99994080ffb9c23e
Vinod Vydier <vvydier@gmail.com>
2017-06-29 20:25:36 -0500

    first commit

---
+++ src/brian-project/src/main/java/com/brian/brianproject/BrianProjectApplication.java
@@ -0,0 +1,12 @@
+package com.brian.brianproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianProjectApplication.class, args);
+	}
+}
